6cac13e2610cf345455f3969527f3a635bcafc7d
adriann1998 <angkajaya98@gmail.com>
2021-05-02 19:51:14 +1000

    moved front end inside backend dir

--- CRM-backend/src/server.js
+++ CRM-backend/src/server.js
@@ -88,8 +88,8 @@ app.delete("/users/:id", users.deleteOne);
 
 // static front end
 if (process.env.NODE_ENV) {
-    app.use(express.static('client/build'));
+    app.use(express.static('crm-frontend/build'));
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'crm-frontend', 'build', 'index.html'))
     })
 }
