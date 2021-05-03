b5b0139712d8e261d396076aa35098a83a23bf7c
adriann1998 <angkajaya98@gmail.com>
2021-05-03 17:18:32 +1000

    renamed api enfpoints

--- server.js
+++ server.js
@@ -41,49 +41,49 @@ Endpoints Configuration
 ---------------------------------- */
 
 // Login ednpoint
-app.post("/login", login);
+app.post("/api/login", login);
 
 //Account RESTFul endpoionts
-app.get("/accounts", authenticateToken, accounts.getAll);
-app.post("/accounts", accounts.createOne);
-app.get("/accounts/:id", accounts.getOne);
-app.put("/accounts/:id", accounts.updateOne);
-app.delete("/accounts/:id", accounts.deleteOne);
+app.get("/api/accounts", authenticateToken, accounts.getAll);
+app.post("/api/accounts", accounts.createOne);
+app.get("/api/accounts/:id", accounts.getOne);
+app.put("/api/accounts/:id", accounts.updateOne);
+app.delete("/api/accounts/:id", accounts.deleteOne);
 
 //Contact RESTFul endpoionts
-app.get("/contacts", authenticateToken, contacts.getAll);
-app.post("/contacts", contacts.createOne);
-app.get("/contacts/:id", contacts.getOne);
-app.put("/contacts/:id", contacts.updateOne);
-app.delete("/contacts/:id", contacts.deleteOne);
+app.get("/api/contacts", authenticateToken, contacts.getAll);
+app.post("/api/contacts", contacts.createOne);
+app.get("/api/contacts/:id", contacts.getOne);
+app.put("/api/contacts/:id", contacts.updateOne);
+app.delete("/api/contacts/:id", contacts.deleteOne);
 
 //Department RESTFul endpoionts
-app.get("/departments", departments.getAll);
-app.post("/departments", departments.createOne);
-app.get("/departments/:id", departments.getOne);
-app.put("/departments/:id", departments.updateOne);
-app.delete("/departments/:id", departments.deleteOne);
+app.get("/api/departments", departments.getAll);
+app.post("/api/departments", departments.createOne);
+app.get("/api/departments/:id", departments.getOne);
+app.put("/api/departments/:id", departments.updateOne);
+app.delete("/api/departments/:id", departments.deleteOne);
 
 //Prospect RESTFul endpoionts
-app.get("/prospects", authenticateToken, prospects.getAll);
-app.post("/prospects", prospects.createOne);
-app.get("/prospects/:id", prospects.getOne);
-app.put("/prospects/:id", prospects.updateOne);
-app.delete("/prospects/:id", prospects.deleteOne);
+app.get("/api/prospects", authenticateToken, prospects.getAll);
+app.post("/api/prospects", prospects.createOne);
+app.get("/api/prospects/:id", prospects.getOne);
+app.put("/api/prospects/:id", prospects.updateOne);
+app.delete("/api/prospects/:id", prospects.deleteOne);
 
 //Quote RESTFul endpoionts
-app.get("/quotes", authenticateToken, quotes.getAll);
-app.post("/quotes", upload.any("files"), quotes.createOne);
-app.get("/quotes/:id", quotes.getOne);
-app.put("/quotes/:id", upload.array("files"), quotes.updateOne);
-app.delete("/quotes/:id", quotes.deleteOne);
+app.get("/api/quotes", authenticateToken, quotes.getAll);
+app.post("/api/quotes", upload.any("files"), quotes.createOne);
+app.get("/api/quotes/:id", quotes.getOne);
+app.put("/api/quotes/:id", upload.array("files"), quotes.updateOne);
+app.delete("/api/quotes/:id", quotes.deleteOne);
 
 //User RESTFul endpoionts
-app.get("/users", users.getAll);
-app.post("/users", users.createOne);
-app.get("/users/:id", users.getOne);
-app.put("/users/:id", users.updateOne);
-app.delete("/users/:id", users.deleteOne);
+app.get("/api/users", users.getAll);
+app.post("/api/users", users.createOne);
+app.get("/api/users/:id", users.getOne);
+app.put("/api/users/:id", users.updateOne);
+app.delete("/api/users/:id", users.deleteOne);
 
 // static front end
 if (process.env.NODE_ENV === "production") {
