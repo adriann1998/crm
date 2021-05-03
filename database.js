53f5cb99e23f84465e8dfa8329e8f4da05ca3ed9
adriann1998 <angkajaya98@gmail.com>
2021-05-03 16:57:36 +1000

    modified babel

--- database.js
+++ database.js
@@ -2,9 +2,8 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  const dbName = 'CRM';
   mongoose.connect(
-    `mongodb+srv://admin:admin@cluster0.pcl3y.mongodb.net/${dbName}?retryWrites=true`,
+    process.env.MONGO_CONN_STRING,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
